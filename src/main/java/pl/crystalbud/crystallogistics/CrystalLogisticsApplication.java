eaccd2b096655544209a42094e92227f5fbb03d0
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-05-06 20:54:35 +0200

    Start

---
+++ src/main/java/pl/crystalbud/crystallogistics/CrystalLogisticsApplication.java
@@ -0,0 +1,13 @@
+package pl.crystalbud.crystallogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrystalLogisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrystalLogisticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/crystalbud/crystallogistics/CrystalLogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.crystalbud.crystallogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrystalLogisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
