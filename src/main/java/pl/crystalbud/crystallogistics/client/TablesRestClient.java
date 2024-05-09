cf8964714de4fafe9233d9a663661ce436df13b6
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-05-09 15:14:04 +0200

    create TablesRestClient.java

---
+++ src/main/java/pl/crystalbud/crystallogistics/client/TablesRestClient.java
@@ -0,0 +1,19 @@
+package pl.crystalbud.crystallogistics.client;
+
+import pl.crystalbud.crystallogistics.entity.Table;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface n TablesRestClient {
+
+    List<Table> findAllTables();
+
+    Table createTable(String title, String details);
+
+    Optional <Table> findTable(int tableId);
+
+    void updateTable(int tableId, String title, String details);
+
+    void deleteTable(int tableId);
+}
