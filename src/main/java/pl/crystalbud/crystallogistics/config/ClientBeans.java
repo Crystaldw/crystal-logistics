25afb17f1477de023a99d791f07d33994eeaebda
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-05-11 19:49:19 +0200

    config SecurityBeans.java

--- src/main/java/pl/crystalbud/crystallogistics/config/ClientBeans.java
+++ src/main/java/pl/crystalbud/crystallogistics/config/ClientBeans.java
@@ -3,6 +3,7 @@ package pl.crystalbud.crystallogistics.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 import pl.crystalbud.crystallogistics.client.RestClientTablesRestClient;
 
@@ -11,9 +12,13 @@ public class ClientBeans {
 
     @Bean
     public RestClientTablesRestClient tablesRestClient(
-            @Value("${crystal_logistics.service.catalogue.uri:http://localhost:8081}") String catalogueBaseUri){
+            @Value("${crystal_logistics.services.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
+            @Value("${crystal_logistics.services.catalogue.username:}") String catalogueUsername,
+            @Value("${crystal_logistics.services.catalogue.password:}") String cataloguePassword){
         return new RestClientTablesRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(
+                        new BasicAuthenticationInterceptor(catalogueUsername, cataloguePassword))
                 .build());
     }
 }
