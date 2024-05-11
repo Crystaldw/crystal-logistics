1650eec1740b5e6ac792d4c1ed75b5beb149c313
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-05-11 14:43:34 +0200

    TableRepository @Query

--- catalogue-service/src/main/java/pl/crystalbud/catalogueservice/repository/TableRepository.java
+++ catalogue-service/src/main/java/pl/crystalbud/catalogueservice/repository/TableRepository.java
@@ -1,10 +1,13 @@
 package pl.crystalbud.catalogueservice.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import pl.crystalbud.catalogueservice.entity.Table;
 
 
 public interface TableRepository extends CrudRepository<Table, Integer> {
 
-    Iterable<Table>findAllByTitleLikeIgnoreCase(String filter);
+    @Query(value = "select p from Table p where p.title ilike :filter")
+    Iterable<Table>findAllByTitleLikeIgnoreCase(@Param("filter") String filter);
 }
